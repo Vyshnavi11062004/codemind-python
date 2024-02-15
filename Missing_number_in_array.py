d284c49499fe0ed6ba0ae313d42c1e06ccce65d3
Somarouthu Vyshnavi <97683007+Vyshnavi11062004@users.noreply.github.com>
2024-02-15 11:31:32 +0530

    t=int(input()) for i in range(t):     n=int(input())     l=list(map(int,input().split()))     s=0     total=n*(n+1)//2     for j in range(n-1):         s+=l[j]     print(total-s)

--- Missing_number_in_array.py
+++ Missing_number_in_array.py
@@ -3,7 +3,7 @@ for i in range(t):
     n=int(input())
     l=list(map(int,input().split()))
     s=0
-    tot=n*(n+1)//2
+    total=n*(n+1)//2
     for j in range(n-1):
         s+=l[j]
-    print(tot-s)
+    print(total-s)
